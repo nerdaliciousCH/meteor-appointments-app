d04cc6f89e45641dd380d2977a885dcb253512b9
Fabian Schneider <fabian@byldr.org>
2024-09-30 16:43:23 +0200

    Some more date util functions

--- imports/utils/date.ts
+++ imports/utils/date.ts
@@ -1,3 +1,15 @@
 export const dateToISODateString = (date: Date): string => {
     return date.toISOString().substring(0, 10);
 };
+
+export const dateToTimestampStrippedTime = (date: Date): number => {
+    return date.getTime() - (date.getTime() % 86400000)
+};
+
+export const getDateToday = (): Date => {
+    return new Date(Date.now());
+}
+
+export const getDateTodayStrippedTime = (): Date => {
+    return new Date(dateToTimestampStrippedTime(getDateToday()));
+};
